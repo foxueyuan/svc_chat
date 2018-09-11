5de6e9941b239d7b87ac68d71ebaa497b5141207
fuhu <fu.hu@live.cn>
2018-09-11 12:19:28 +0800

    optimization

--- handler/chat.py
+++ handler/chat.py
@@ -36,42 +36,34 @@ async def chat(request):
             data['text'] = text_rst['content']
 
     # 文本审核，违禁处理
-    spam_resp = await request_spam(conf.SVC_SPAM_URL, data['text'])
-    if spam_resp.get('spam', 0):
-        result['data'] = {'msgtype': 'text',
-                          'text': data['text'],
-                          'content': {'text': '我们都是文明人，怎么可以说这样的话。'}}
-        return response.json(result)
+    # spam_resp = await request_spam(conf.SVC_SPAM_URL, data['text'])
+    # if spam_resp.get('spam', 0):
+    #     result['data'] = {'msgtype': 'text',
+    #                       'text': data['text'],
+    #                       'content': {'text': '我们都是文明人，怎么可以说这样的话。'}}
+    #     return response.json(result)
 
-    # 语义解析，初步识别意图
-    wordcom_resp = await request_wordcom(conf.SVC_WORDCOM_URL, data['text'])
-    if wordcom_resp.get('intent') == 1:
-        for con_token in wordcom_resp.get('com_tokens', []):
-            if con_token['com_type'] == 7 and (con_token['com_word'] != '深圳' or '深圳' in data['text']):
-                # 处理天气问答，涉及到具体地理，后续多轮对话实现
-                weather_answer = await request_small_talk(conf.SVC_SMALL_TALK_URL, data['text'])
-                result['data'] = {'msgtype': 'text',
-                                  'text': data['text'],
-                                  'content': {'text': weather_answer}}
-
-        if 'data' not in result:
-            result['data'] = {'msgtype': 'text',
-                              'text': data['text'],
-                              'content': {'text': '需要说明查询地区，您可以这么问："长沙今天天气怎样"'}}
-        return response.json(result)
-
-    kg_rst = await request_unit(conf.SVC_UNIT_CHAT_URL, data['text'])
-    if kg_rst['errcode'] == 0:
-        for action in kg_rst['result']['response']['action_list']:
+    rst = await request_unit(conf.SVC_UNIT_CHAT_URL, data['text'])
+    if rst['errcode'] == 0:
+        for action in rst['result']['response']['action_list']:
             if action['type'] == 'satisfy':
-                if action['action_id'] == 'faq_instruction_satisfy':
-                    pass
                 if action['say'].startswith('#instruction#'):
                     content = json.loads(action['say'].strip('#instruction#'))
                     if content['action'] == 'time':
                         result['data'] = {'msgtype': 'instruction',
                                           'text': data['text'],
                                           'content': time.strftime('%Y年%d月%m日 %H点%M分%S秒', time.localtime())}
+                    elif content['action'] == 'weather':
+                        for lexical in rst['result']['response']['qu_res']['lexical_analysis']:
+                            if lexical['type'] == '23':
+                                weather_answer = await request_small_talk(conf.SVC_SMALL_TALK_URL, data['text'])
+                                result['data'] = {'msgtype': 'text',
+                                                  'text': data['text'],
+                                                  'content': {'text': weather_answer}}
+                        if 'data' not in result:
+                            result['data'] = {'msgtype': 'text',
+                                              'text': data['text'],
+                                              'content': {'text': '需要说明查询地区，您可以这么问："长沙今天天气怎样"'}}
                     else:
                         result['data'] = {'msgtype': 'instruction',
                                           'text': data['text'],
@@ -80,12 +72,14 @@ async def chat(request):
                     result['data'] = {'msgtype': 'text',
                                       'text': data['text'],
                                       'content': {'text': action['say']}}
+
                 if action['action_id'] == 'faq_qa_satisfy':
                     log_task.send(data['text'], action['say'], 'qa')
                 elif action['action_id'] == 'faq_kg_satisfy':
                     log_task.send(data['text'], action['say'], 'kg')
                 elif action['action_id'] == 'faq_instruction_satisfy':
                     log_task.send(data['text'], action['say'], 'instruction')
+
                 return response.json(result)
 
     # 匹配不到进入闲聊模式
